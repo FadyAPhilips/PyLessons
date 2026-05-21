a4db0e28622c76029fc0810f4029d8034c542b90
FadyAPhilips <fadyaphilips97@gmail.com>
2026-05-20 23:17:40 -0400

    Commited some previous lessons

---
+++ 26-05-19/chess.py
@@ -0,0 +1,45 @@
+board = [
+    [0,0,0,0,0,0,0,0],
+    [0,0,0,"W_K",0,0,0,0],
+    [0,"B_R",0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0],
+    ["W_B",0,0,0,0,0,0,"B_N"],
+    [0,"W_P",0,0,0,0,0,0],
+    [0,0,"B_K",0,0,0,0,0],
+    [0,0,0,0,0,0,0,0]
+]
+
+boardl = ["R1", "R2", "R3", "R4", "R5", "R6", "R7", "R8"]
+
+# BK = board[6][2]
+
+# print(BK)
+
+# # BR =
+
+for i in range(len(board)):
+    print(i)
+
+#     # for square in row:
+#     #     if square == "B_R":
+#     #         print("found")
+
+# len(boardl) == 8
+
+# range(8) == [0,1,2,3,4,5,6,7]
+
+# for i in range(8):
+#     print(i)
+
+
+list1 = ["A", "B", "C", "D", "E", "F"]
+
+# for i in range(len(list1)):
+#     print(i)
+    
+# for i in list1:
+#     print(i)
+
+# print(len(board))
+
+# pos4 = list1[4]
